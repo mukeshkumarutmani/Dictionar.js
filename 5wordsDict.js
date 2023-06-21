7429172af3bb1ae70575c637cc91eaa31b6cce57
[Mukesh Kumar Utmani] <[kumarmukesh789123@gmail.com]>
2023-06-21 10:18:10 +0500

    'mukesh'

---
+++ 5wordsDict.js
@@ -0,0 +1,12 @@
+const dict ={
+    wisdom: "who have wisdom",
+    Athleisure:"Comfortable and casual footwear",
+    Broigus:"Angry or irritated.",
+    Catch:	"A catch of fish that cannot be put", 
+    Blert:	"A cowardly person, someone who is weak.",
+  }
+  console.log(dict);
+  // 2nd method:
+  console.log(dict['wisodm']);
+
+
